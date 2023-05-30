c8de023f6b950c95b702b82ff485265dd75376c1
FaridIzquierdo <u202015215@upc.edu.pe>
2023-05-30 00:04:51 -0500

    Implement UserRepository and UserNotFoundException

---
+++ src/main/java/com/eventxplorer/EventXplorer/API/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.eventxplorer.EventXplorer.API.repository;
+
+import com.eventxplorer.EventXplorer.API.model.User;
+import org.springframework.data.repository.CrudRepository;
+
+public interface UserRepository extends CrudRepository<User, Integer>{
+
+
+}
